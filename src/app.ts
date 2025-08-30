913fcc830cfbc42c1dcb31f5e992d97c01bdb781
kresd <kresnamalyuda7@gmail.com>
2025-08-30 11:59:40 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import expressLayouts from "express-ejs-layouts";
 import path from "path";
-// import { router } from "@routes/index";
-// import { authGuard } from "@middleware/auth";
+import { router } from "./routes/index";
+import { authGuard } from "./middleware/auth";
 
 dotenv.config();
 
@@ -24,8 +24,7 @@ app.use(expressLayouts);
 app.set("layout", "layouts/main");
 
 // Routes
-// app.use(authGuard);
-// app.use("/", router);
-app.get("/", (req, res) => res.send("Hello from Express on Vercel!"));
+app.use(authGuard);
+app.use("/", router);
 
 export default app;
