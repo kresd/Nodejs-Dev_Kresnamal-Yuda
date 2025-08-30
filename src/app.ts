ff63189f97befe6dd372a302af36ab40e2ec62df
kresd <kresnamalyuda7@gmail.com>
2025-08-30 11:55:57 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,12 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Public folder
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(process.cwd(), "public")));
 
-// Views
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../views"));
+app.set("views", path.join(process.cwd(), "views"));
+
 
 app.use(expressLayouts);
 app.set("layout", "layouts/main");
