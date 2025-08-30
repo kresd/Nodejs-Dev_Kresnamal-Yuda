3cbfc76326dab29365ed5ba20a449bb29d910f05
kresd <kresnamalyuda7@gmail.com>
2025-08-30 15:33:40 +0700

    Update app.ts

--- src/routes/feature/index.ts
+++ src/routes/feature/index.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import Controller from "../../controllers/feature";
+import { requiredAuth } from "@middleware/auth";
 
 const router = Router();
 
-router.get("/compare", Controller.getCompareForm);
-router.post("/compare", Controller.compareInputs);
-router.get("/compare/history", Controller.getComparisonHistory);
-router.post("/compare/history/delete/:id", Controller.deleteComparisonHistory);
+router.get("/compare", requiredAuth, Controller.getCompareForm);
+router.post("/compare", requiredAuth, Controller.compareInputs);
+router.get("/compare/history", requiredAuth, Controller.getComparisonHistory);
+router.post(
+  "/compare/history/delete/:id",
+  requiredAuth,
+  Controller.deleteComparisonHistory
+);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import featureRoute from "./feature";
 import authRoute from "./auth";
-import { requiredAuth } from "../middleware/auth";
 
 const router = Router();
 
 router.use("/", authRoute);
-router.use("/features", requiredAuth, featureRoute);
+router.use("/features", featureRoute);
 
 export { router };
