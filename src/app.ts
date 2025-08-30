7d49c997a89f638e4781680d5c6f31f6e4fdef50
kresd <kresnamalyuda7@gmail.com>
2025-08-30 10:59:06 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,33 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import expressLayouts from "express-ejs-layouts";
-// import { router } from "@routes/index";
-// import { authGuard } from "@middleware/auth";
 import path from "path";
+
+import { router } from "./routes/index";
+import { authGuard } from "./middleware/auth";
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.set("view engine", "ejs");
-app.set("views", "./src/views");
-
+app.set("views", path.join(__dirname, "../views"));
 app.use(expressLayouts);
-app.set("layout", "layouts/main"); 
+app.set("layout", "layouts/main");
 
-// app.use(authGuard);
-// app.use("/", router);
-app.get("/", (req, res) => res.send("Hello from Express on Vercel!"));
-
+app.use(authGuard);
+app.use("/", router);
 
 export default app;
